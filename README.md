dfe3ada3b5c87249283d109a312b83c1c10980b9
Arne Bouillon <arne.bouillon@kuleuven.be>
2023-08-18 15:52:36 +0200

    Refactor code

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 Git repo for super android project. Distributed computing on android phones.
 
-# Prefetching everything on your machine
+# Table of available applications
+| Name | Description |
+| ---- | ----------- |
+| `julia_set` | Compute the Julia set |
 
-First additionally clone the submodules:
+# Cloning this repository
+Clone this repository and got to its root folder. Then clone the submodules with
 ```sh
 git submodule init
 git submodule update
@@ -42,8 +46,8 @@ Then ssh into the phone
 and grant SimpleSSHD super user rights in the Magisk popup on the phone.
 
 While ssh'd into the phone, run  
-	`g++ julia.c`  
-to generate an executable.
+	`g++ <application>_client.c -o <application>_client`  
+to generate an executable. Here, the `<application>` can be any of the applications in the [table](#table-of-available-applications).
 
 And/or,
 	`offline_build.sh`
@@ -51,4 +55,11 @@ from within `super-pi-ray`.
 
 # Running a compute
 
-TODO.
+### Set up communication with the phones
+Create an SSH key pair of type `ed25519` (`ssh-keygen -t ed25519`) and ask someone to place the public key on each of the phones. Then create a file named `.pkey` (which is ignored by Git) in the root of this repository, containing the **path to** your private key.
+
+### Run the application server
+Ensure Python 3 is installed on your machine. Then choose an application from the [table](#table-of-available-applications) and run
+```python3
+python3 <application>_server.py
+```
--- julia_set_server.py
+++ julia_set_server.py
@@ -1,19 +1,12 @@
 import numpy as np
 import itertools
 import random
-import threading
 import matplotlib.pyplot as plt
 import matplotlib.cm as cm
 
-phones = [
-    "192.168.0.2",
-    "192.168.0.5",
-    "192.168.0.6",
-    "192.168.0.7",
-    "192.168.0.8",
-    # Protip: don't use the adress 192.168.0.1
-    ]
-nbr_phones = len(phones)
+import server
+
+nbr_phones = len(server.PHONES)
 
 # Image width and height in pixels; parameters for the plot
 width, height = 200 * nbr_phones, 200 * nbr_phones # Make sure these are divisble by nbr_phones
@@ -43,30 +36,17 @@ def julia(z, c=complex(-0.1, 0.65)):
 
     return ratio
 
-import paramiko
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-
 # Initialize Julia set
 J = np.zeros((width, height))
 
-class FractalThread (threading.Thread):
+class FractalThread (server.PhoneThread):
     def __init__(self, phone_index, width_, height_, coords):
         self.phone_index, self.width_, self.height_, self.coords = phone_index, width_, height_, coords
-        threading.Thread.__init__(self)
+        super().__init__(phone_index, 'julia_set', [width_, height_, *coords])
 
     def run(self):
-        print(f"Starting {phones[self.phone_index]}")
-        phone_index, width_, height_, coords = self.phone_index, self.width_, self.height_, self.coords
-
-        ssh = paramiko.SSHClient()
-        ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        # ssh.connect(f"192.168.0.{number}", port=2222, pkey=paramiko.Ed25519Key.from_private_key_file("/home/arne/.ssh/id_phones"))
-        ssh.connect(f"{phones[phone_index]}", port=2222, pkey=paramiko.Ed25519Key.from_private_key_file("/home/simonj/.ssh/id_ed25519"))
-        _, out, _ = ssh.exec_command(f"su -c './a.out {width_} {height_} {' '.join(map(str, coords))}'")
-
-        J[(phone_index * width_):((phone_index + 1) * width_), :] = np.reshape([float(s) for s in out.read().splitlines()], (width_, height_))
-        print(f"Ending {phones[self.phone_index]}")
+        res = super().run()
+        J[(self.phone_index * self.width_):((self.phone_index + 1) * self.width_), :] = np.reshape([float(s) for s in res.read().splitlines()], (self.width_, self.height_))
 
 threads = [
     FractalThread(
